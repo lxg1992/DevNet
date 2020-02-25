62a9c31b115521fef632d4f563901d32a7673be7
Alexey <lxg1992@gmail.com>
2020-02-25 18:45:08 -0500

    Define User Schema

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express");
+
 const connectDB = require("./config/db");
+const authRoute = require("./routes/api/auth");
+const usersRoute = require("./routes/api/users");
+const postsRoute = require("./routes/api/posts");
+const profileRoute = require("./routes/api/profile");
 
 //Initialize express server
 const app = express();
@@ -12,6 +17,15 @@ const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
     res.send("Test");
 });
+
+app.use("/api/users", usersRoute);
+app.use("/api/posts", postsRoute);
+app.use("/api/profile", profileRoute);
+app.use("/api/auth", authRoute);
+
+//Define routes
+//app.use("/api/users");
+
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
