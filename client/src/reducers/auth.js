4879b8a8cca80f6d1223e45fdab5a4c6e5a60e34
Alexey <lxg1992@gmail.com>
2020-03-31 20:59:35 -0400

    User Login Redux

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -2,7 +2,9 @@ import {
     REGISTER_SUCCESS,
     REGISTER_FAIL,
     USER_LOADED,
-    AUTH_ERROR
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL
 } from "../actions/types";
 
 const initialState = {
@@ -30,8 +32,17 @@ export default function(state = initialState, action) {
                 isAuthenticated: true,
                 loading: false
             };
+        case LOGIN_SUCCESS:
+            localStorage.setItem("token", payload.token);
+            return {
+                ...state,
+                ...payload,
+                isAuthenticated: true,
+                loading: false
+            };
         case REGISTER_FAIL:
         case AUTH_ERROR:
+        case LOGIN_FAIL:
             localStorage.removeItem("token");
             return {
                 ...state,
